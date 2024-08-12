dae51307fbcdf0d43ba977fa1c32a55521576e3d
hipopotamus <qkqntjddn@gmail.com>
2024-08-12 16:24:33 +0900

    init

---
+++ src/test/java/video/videoservice/VideoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package video.videoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
