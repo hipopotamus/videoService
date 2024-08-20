bade3007931a690b63c0d5b9509f87e27a59d27e
hipopotamus <qkqntjddn@gmail.com>
2024-08-20 20:16:38 +0900

    데이터 추가 컨트롤러 구현

---
+++ src/main/java/videoservice/global/test/DataGenerateController.java
@@ -0,0 +1,41 @@
+package videoservice.global.test;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dataGenerate")
+@RequiredArgsConstructor
+public class DataGenerateController {
+
+    private final DataGenerateService dataGenerateService;
+
+    @PostMapping("/account/{size}")
+    public ResponseEntity<String> generateAccount(@PathVariable long size) {
+
+        dataGenerateService.generateAccount(size);
+
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
+
+    @PostMapping("/board/{size}")
+    public ResponseEntity<String> generateBoard(@PathVariable long size) {
+
+        dataGenerateService.generateBoard(size);
+
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
+
+    @PostMapping("/daily")
+    public ResponseEntity<String> generateDaily() {
+
+        dataGenerateService.generateDailyStats();
+
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
+}
